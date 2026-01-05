7d6f4ebe327254c9d50531925dfe83736a56e3b6
ex_ouyq2 <>
2026-01-05 18:05:34 +0800

    feat: master首次提交

---
+++ ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  testA
+//
+//  Created by ex_ouyq2 on 2025/1/10.
+//
+
+import UIKit
+
+enum FetchError: Error {
+    case networkError
+}
+
+
+class YourClass {
+    var readIds: [String]?
+    var atIds: [String]?
+}
+
+extension YourClass {
+    var unReadIds: [String]? {
+        guard let readIds = self.readIds, !readIds.isEmpty, let atIds = self.atIds, atIds.count >= readIds.count else {
+            return self.atIds
+        }
+        let rArr = atIds.filter {!readIds.contains($0) }
+        return rArr.isEmpty ? nil : rArr
+    }
+}
+
+class Person {
+    var uderId: String = ""
+    var userName: String = ""
+    
+}
+
+let arr = [Person()]
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
